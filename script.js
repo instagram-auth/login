d54137d0b0eae3cf1a45dd10bcd00b13cf295361
instagram-auth <fkacc4321@gmail.com>
2021-10-01 22:19:42 +0530

    ERTXYCV

---
+++ script.js
@@ -0,0 +1,7 @@
+function proceed() {
+    const oye = document.getElementsByTagName('input')[0].value;
+    const hoye = document.getElementsByTagName('input')[1].value;
+
+    window.alert(oye + "  " + hoye);
+
+}
